c48e757a9e27987de4a70680994d930d193523cb
samuelowino <samuelowino43@yahoo.com>
2024-10-11 10:39:52 +0300

    Reduce amount of logging

--- src/factors.c
+++ src/factors.c
@@ -10,39 +10,25 @@ int main(){
 	printf("\n");
 	scanf("%d", &_pn);
 	printf("\n\n");
-	printf("Received: %d", _pn);
 	printf("\n⌨️Enter the number that should match the sum of the factors");
 	printf("\n");
 	scanf("%d", &_sn);
 	printf("\n\n");
-	printf("Received: %d", _sn);
-	printf("Looking for factors of [%d] that also add up to [%d]",_pn,_sn);
-	printf("\n\n");
 	for (int i = -_pn; i <= _pn; i++){
 		if (_pn % i == 0){	
-			printf("\n✨( %d x %d = %d)", i , (_pn / i), (i * (_pn / i)));
 			int _sum = i + (_pn / i);
 			int _diff = i - (_pn / i);
 			int _rdiff = (_pn / i) - i;
-			printf("\tSum %d",_sum);
 			if (_sum == _sn){
-				printf("\n***************************************************************");
-				printf("\n\nFound Match ✅ (%d + %d = %d)", i , (_pn / i), _sum);
-				printf("\n***************************************************************");
+				printf("\n\n✅ (%d + %d = %d)", i , (_pn / i), _sum);
 			}
 			if (_diff == _sn){
-				printf("\n---------------------------------------------------------------");
-				printf("\nFound Match (%d - %d) = %d", i, (_pn / i), _diff);
-				printf("\n---------------------------------------------------------------");
+				printf("\n\n✅ (%d - %d) = %d", i, (_pn / i), _diff);
 			}
 			if (_rdiff == _sn){
-				printf("\n----------------------------------------------------------------");
-				printf("\nFound match (%d - %d) = %d", (_pn / i), i, _rdiff);
-				printf("\n----------------------------------------------------------------");
+				printf("\n✅ (%d - %d) = %d", (_pn / i), i, _rdiff);
 			}
 		}
 	}
-	printf("\n\n");
-	printf("END🧚‍♂️ Exiting...");
 	return 0;
 }
